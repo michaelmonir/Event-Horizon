73c17daaf774e94c6ba4491ec9d86b71292efbe7
Ahmed Hassan <ahmedmohamedhassan1to10@gmail.com>
2023-11-09 23:39:01 +0200

    intial project

---
+++ src/main/java/com/EventHorizon/EventHorizon/EventHorizonApplication.java
@@ -0,0 +1,13 @@
+package com.EventHorizon.EventHorizon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHorizonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHorizonApplication.class, args);
+	}
+
+}
