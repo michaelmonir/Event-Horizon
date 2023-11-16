d2bc271ef51b94dca231b6c588c8261291e8848c
Ahmed Hassan <ahmedmohamedhassan1to10@gmail.com>
2023-11-16 04:24:12 +0200

    some change server of mail verification

--- src/main/java/com/EventHorizon/EventHorizon/MailSender/MailConfig.java
+++ src/main/java/com/EventHorizon/EventHorizon/MailSender/MailConfig.java
@@ -13,7 +13,7 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587); // use the appropriate port
         mailSender.setUsername("aher70740@gmail.com");
-        mailSender.setPassword("utlf calc qlef dtcp");
+        mailSender.setPassword("fjws bljs kwro sgns");
         java.util.Properties properties = new java.util.Properties();
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.enable", "true");
