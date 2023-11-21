4fd0c08d78558d87ea1be91fc54f342b04d51d2e
moriyad12 <mohamedelhenawy2012@yahoo.com>
2023-11-21 13:24:08 +0200

    edit dashboard to service

--- src/main/java/com/EventHorizon/EventHorizon/Dashboard/Dashboard.java
+++ src/main/java/com/EventHorizon/EventHorizon/Dashboard/Dashboard.java
@@ -9,10 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Repository
+@Service
 public class Dashboard {
     private List<EventHeaderDto> eventHeaderDtos;
     @Autowired
