bd3eec41149bcdfec5748805476922bda1280df5
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-11-28 19:36:39 +0200

    Update VerifyRequest.java

--- src/main/java/com/EventHorizon/EventHorizon/security/authenticationMessages/VerifyRequest.java
+++ src/main/java/com/EventHorizon/EventHorizon/security/authenticationMessages/VerifyRequest.java
@@ -1,10 +1,14 @@
 package com.EventHorizon.EventHorizon.security.authenticationMessages;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VerifyRequest {
     private String token;
     private String verifyCode;
