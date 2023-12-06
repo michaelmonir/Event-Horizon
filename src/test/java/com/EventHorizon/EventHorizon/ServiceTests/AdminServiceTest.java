5354affe0dbe34e8a6a74a9a02f884ac0ac5a709
faris <113785816+faris0007@users.noreply.github.com>
2023-12-06 05:54:03 +0200

    Create Service to admin and moderator

---
+++ src/test/java/com/EventHorizon/EventHorizon/ServiceTests/AdminServiceTest.java
@@ -0,0 +1,42 @@
+package com.EventHorizon.EventHorizon.ServiceTests;
+
+import com.EventHorizon.EventHorizon.Entities.UserEntities.Information;
+import com.EventHorizon.EventHorizon.Entities.enums.Role;
+import com.EventHorizon.EventHorizon.entity.InformationCreator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AdminServiceTest {
+
+
+    @Autowired
+    InformationCreator informationCreator;
+
+
+
+    @Test
+    public void addModeratorTestIfSucceed() {
+
+
+    }
+
+    @Test
+    public void addModeratorTestIfFail() {
+
+
+    }
+
+    @Test
+    public void deleteModeratorTestIfSucceed() {
+
+
+    }
+
+    @Test
+    public void deleteModeratorTestIfFail() {
+
+
+    }
+}
---
+++ src/test/java/com/EventHorizon/EventHorizon/ServiceTests/AuthorityServiceTest.java
@@ -0,0 +1,11 @@
+package com.EventHorizon.EventHorizon.ServiceTests;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthorityServiceTest {
+
+
+
+
+}
